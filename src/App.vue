c1501a08c44c7ea36a28e3a3587ccc2cb8b0b573
camaleoncoce <42813694+camaleoncoce@users.noreply.github.com>
2020-09-06 13:31:24 -0600

    archivos iniciales

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" :clipped="$vuetify.breakpoint.lgAndUp" app>
+      <v-list dense></v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
+        <span class="hidden-sm-and-down">TEST UFG</span>
+      </v-toolbar-title>
+     <!-- <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down"
+      ></v-text-field>-->
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+      <v-btn icon large>
+        <v-avatar size="32px" item>
+          <v-img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify"></v-img>
+        </v-avatar>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-tooltip right>
+            <template v-slot:activator="{ on }">
+              <v-btn :href="source" icon large target="_blank" v-on="on">
+                <v-icon large>mdi-code-tags</v-icon>
+              </v-btn>
+            </template>
+            <span>Source</span>
+          </v-tooltip>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <!--------------- componente del footer----------->
+    <v-footer color="primary lighten-1" padless>
+      <v-row justify="center" no-gutters>
+      
+        <v-col class="blue darken-4 lighten-2 py-4 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} —
+          <strong>UFG</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+    <!-- <v-btn bottom color="pink" dark fab fixed right @click="dialog = !dialog">
+      <v-icon>add</v-icon>
+    </v-btn>-->
+  </v-app>
+</template>
+
+<script>
+export default {
+   props: {
+      source: String,
+    },
+  name: "panaderia",
+   
+  data() {
+    
+    return {
+      drawer: null,
+
+    };
+  }
+};
+</script>

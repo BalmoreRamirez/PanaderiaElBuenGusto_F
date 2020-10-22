4041d669a03f6ab2f147dea26c78ff83a069646e
camaleoncoce <42813694+camaleoncoce@users.noreply.github.com>
2020-10-21 22:12:40 -0600

    finalizando la vista de inventario

--- src/components/Inventario.vue
+++ src/components/Inventario.vue
@@ -7,6 +7,9 @@
           sort-by="calories"
           class="elevation-1"
       >
+        <template v-slot:item.Disponible="{item}">
+          <v-chip :color="getColor(item.Disponible)" dark>{{ item.Disponible }}</v-chip>
+        </template>
         <template v-slot:top>
           <v-toolbar flat color="white">
             <v-toolbar-title>{{ inventario }}</v-toolbar-title>
@@ -29,15 +32,23 @@ export default {
       inventario: 'Inventario',
       headers: [
         {text: 'ID', value: 'IdInventario',},
-        {text: 'REGISTRO', value: 'RegistroMPID'},
+        {text: 'REGISTRO', value: 'NombreMP'},
         {text: 'DISPONIBLE', value: 'Disponible'},
-        {text: 'BODEGA', value: 'BodegaID'},
+        {text: 'BODEGA', value: 'NombreBodega'},
         {text: 'FECHA INGRESO', value: 'FechaIngreso'},
       ],
       Inventarios: []
     }
   },
   methods: {
+    //si la cantidad disponible es menor o igual que cuatro se coloca en rojo
+    //si la cantidad disponibles es mayor a 5 se ponen en verde
+    getColor(Disponible) {
+      if (Disponible >= 5) return 'green'
+      else if (Disponible <= 4) return 'red'
+    },
+
+    //se trae los datos de inventario y se transforma en json y se guarda en un array
     async getInventario() {
       try {
         const data = await fetch('http://localhost/PanaderiaBG/public/Inventario')
@@ -48,8 +59,12 @@ export default {
       }
     }
   },
+
+  //para que el navegador lo recargue antes que el componente
   created() {
     this.getInventario()
   },
 }
 </script>
+
+
